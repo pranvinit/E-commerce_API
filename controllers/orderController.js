8cd21a851343fbb95f6e230155c558f020479471
Pranav <pranvofficial@gmail.com>
2022-01-20 00:36:39 +0530

    added order controllers and stripe integration pattern

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,24 +1,112 @@
+const Order = require("../models/Order");
+const Product = require("../models/Product");
 const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
+const { checkPermissions } = require("../utils");
+
+// stripe API provides a client_secret
+const fakeStripeAPI = async ({ amount, currency }) => {
+  const client_secret = "secret_value";
+  return { client_secret, amount };
+};
 
 const getAllOrders = async (req, res) => {
-  res.send("get all orders");
+  const orders = await Order.find({});
+  res.status(StatusCodes.OK).json({ orders, nbHits: orders.length });
 };
 
 const getSingleOrder = async (req, res) => {
-  res.send("get single order");
+  const order = await Order.findOne({ _id: req.params.id });
+  if (!order) {
+    throw new CustomError.NotFoundError(`No order with id: ${req.params.id}`);
+  }
+  checkPermissions(req.user, order.user);
+  res.status(StatusCodes.OK).json({ order });
 };
 
 const getCurrentUserOrders = async (req, res) => {
-  res.send("get current user orders");
+  const orders = await Order.find({ user: req.user.userId });
+  res.status(StatusCodes.OK).json({ orders, nbHits: orders.length });
 };
 
+// executes when user hits the checkout page
+
 const createOrder = async (req, res) => {
-  res.send("create order");
+  const { items: cartItems, tax, shippingFee } = req.body;
+  if (!cartItems || cartItems.length < 1) {
+    throw new CustomError.BadRequestError("No cart items provided");
+  }
+  if (!tax || !shippingFee) {
+    throw new CustomError.BadRequestError(
+      "Please provide tax and shipping fee"
+    );
+  }
+  // array of order items fetched from database
+  let orderItems = [];
+  // total aggregate amount of order items fetched from database
+  let subtotal = 0;
+
+  // 'for of' so we can use async functionality
+  for (const item of cartItems) {
+    // fetching product form db with id provided from frontend
+    const dbProduct = await Product.findOne({ _id: item.product });
+    if (!dbProduct) {
+      throw new CustomError.NotFoundError(
+        `No product with id : ${item.product}`
+      );
+    }
+    const { name, price, image, _id } = dbProduct;
+    // order item fetched from database
+    const singleOrderItem = {
+      amount: item.amount,
+      name,
+      price,
+      image,
+      product: _id,
+    };
+    // adds item to array
+    orderItems = [...orderItems, singleOrderItem];
+    // calculate subtotal
+    subtotal += item.amount * price;
+  }
+  // calculate total
+  const total = tax + shippingFee + subtotal;
+
+  // send amount and currency to stripe API
+  // returns client secret
+  const paymentIntent = await fakeStripeAPI({
+    amount: total,
+    currency: "rupee",
+  });
+
+  const order = await Order.create({
+    orderItems,
+    total,
+    subtotal,
+    tax,
+    shippingFee,
+    clientSecret: paymentIntent.client_secret,
+    user: req.user.userId,
+  });
+  res
+    .status(StatusCodes.CREATED)
+    .json({ order, clientSecret: order.clientSecret });
 };
 
 const updateOrder = async (req, res) => {
-  res.send("update order");
+  const { paymentIntentId } = req.body;
+
+  const order = await Order.findOne({ _id: req.params.id });
+  if (!order) {
+    throw new CustomError.NotFoundError(`No order with id: ${req.params.id}`);
+  }
+  checkPermissions(req.user, order.user);
+  // stripe API sends the payment intent id on the frontend
+  // add the it to order instance once payment is received and change status to 'paid'
+  order.paymentIntent = paymentIntentId;
+  order.status = "paid";
+  await order.save();
+  res.status(StatusCodes.OK).json({ order });
 };
 
 module.exports = {
